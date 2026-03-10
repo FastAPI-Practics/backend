e7dd7366c53cb7943371ddf2795748cedb6144df
Виталий Дубровец <vitdub2@gmail.com>
2026-03-10 17:28:33 +0300

    auth

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,15 +3,36 @@ from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    db_schema: str = 'postgresql+asyncpg'
-    db_host: str = 'localhost'
-    db_user: str = 'postgres'
-    db_password: str = 'pass'
-    db_port: int = 5432
-    db_name: str = 'db'
+class AuthSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix='AUTH_',
+        env_file='.env',
+        extra='ignore',
+    )
 
+    secret: str = 'secret'
+    token_lifetime_seconds: int = 300
+    token_algorithm: str = 'HS256'
+
+class DBSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix='DB_',
+        env_file='.env',
+        extra='ignore',
+    )
+
+    protocol: str = 'postgresql+asyncpg'
+    host: str = 'localhost'
+    user: str = 'postgres'
+    password: str = 'pass'
+    port: int = 5432
+    name: str = 'db'
+
+class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env')
+
+    db: DBSettings = DBSettings()
+    auth: AuthSettings = AuthSettings()
 
 
 @lru_cache
