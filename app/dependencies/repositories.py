a0b14c1ee4ef288f134393b757b1b3dc6aaec1b3
Виталий Дубровец <vitdub2@gmail.com>
2026-03-10 13:48:23 +0300

    repositories fixes

--- app/dependencies/repositories.py
+++ app/dependencies/repositories.py
@@ -12,14 +12,12 @@ async def get_user_repository(session: SessionDep):
     yield Repository[UserModel](session)
 
 
-
 type UserRepository = Repository[UserModel]
 UserRepositoryDep = Annotated[UserRepository, Depends(get_user_repository)]
 
 
 async def get_pet_repository(session: SessionDep):
     yield Repository[PetModel](session)
-
 
 
 type PetRepository = Repository[PetModel]
