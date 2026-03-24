8d410945c1a496f01218db20ec2f95af4316ce43
lamarr <test.vitdub@gmail.com>
2026-03-24 15:53:27 +0300

    feat(errors): error formatting

--- app/routers/pets.py
+++ app/routers/pets.py
@@ -2,20 +2,20 @@ from typing import Annotated, Sequence
 
 from fastapi import APIRouter, Query
 
+from app.core.responses import auth_responses
+from app.core.security import AccessTokenDep
 from app.dependencies.repositories import PetRepositoryDep
 from app.models.pets import PetModel
 from app.schemas.base import CommonListFilters
 
-router = APIRouter(
-    prefix='/pets',
-    tags=['pets'],
-)
+router = APIRouter(prefix='/pets', tags=['pets'], responses=auth_responses)
 
 
 @router.get('/')
 async def get_pets(
     pet_repository: PetRepositoryDep,
     filters: Annotated[CommonListFilters, Query()],
+    _: AccessTokenDep,
 ) -> Sequence[PetModel]:
     return await pet_repository.fetch(
         offset=filters.offset,
@@ -24,5 +24,9 @@ async def get_pets(
 
 
 @router.post('/')
-async def create_pets(pet: PetModel, pet_repository: PetRepositoryDep) -> PetModel:
+async def create_pets(
+    pet: PetModel,
+    pet_repository: PetRepositoryDep,
+    _: AccessTokenDep,
+) -> PetModel:
     return await pet_repository.save(pet)
